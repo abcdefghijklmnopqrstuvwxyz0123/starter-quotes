c099b5ccb5fdd3ed90bc1f423ed69496878406ec
Andrew Lam <atlam995@gmail.com>
2017-09-15 19:04:33 -0700

    Updated actor.php & updated changelog.md Signed-off-by:Andrew Lam <atlam995@gmail.com>

--- application/views/actor.php
+++ application/views/actor.php
@@ -1,7 +1,7 @@
 <div class="row">
         <div class="span4">
                 <a href="/welcome/show/{key}">
-                        <img src="/pix/{who}.jpg" title="{who} plays {role}"/>
+                        <img src="/pix/{who}.jpg" title="{who} plays as {role}"/>
                 </a>
         </div>
 </div>
