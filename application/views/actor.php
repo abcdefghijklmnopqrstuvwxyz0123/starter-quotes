0ddd8556fbce38a11ad18618c58bdea9ac67798c
Andrew Lam <atlam995@gmail.com>
2017-09-15 19:10:46 -0700

    Push test Signed-off-by:Andrew Lam <atlam995@gmail.com>

--- application/views/actor.php
+++ application/views/actor.php
@@ -3,5 +3,6 @@
                 <a href="/welcome/show/{key}">
                         <img src="/pix/{who}.jpg" title="{who} plays as {role}"/>
                 </a>
+            
         </div>
 </div>
